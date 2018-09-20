cab123f3b1154165d1a1361c4dc5aa9583529a8c
Herrelaekker <40020645+Herrelaekker@users.noreply.github.com>
2018-09-20 21:41:51 +0200

    Hop + playermovement Kommentarer + Fixed
    Code fix
    Hoppeknapper i stedet for kun Mellemrum
    Kommentarer added i playermovement

--- Platformer med unik artstyle/Assets/PlayerMovement.cs
+++ Platformer med unik artstyle/Assets/PlayerMovement.cs
@@ -27,14 +27,17 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+        //bevæger sig hele tiden anpå moveInput
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed , rb.velocity.y);
     }
 
     void Update()
     {
+        //isGrounded er hvis der er Ground under en
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
+        //Hvis man går til højre, vend til højre | Hvis man går til venstre, vend til venstre
         if (moveInput > 0)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
@@ -44,6 +47,7 @@ public class PlayerMovement : MonoBehaviour {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
 
+        //Hvis man bevæger sig på ground -> Gå animation, ellers Idle animation
         if(isGrounded == true && moveInput != 0)
         {
             anim.SetInteger("state", 1);
@@ -51,7 +55,8 @@ public class PlayerMovement : MonoBehaviour {
         else
             anim.SetInteger("state", 0);
 
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        //Hvis man trykker hop, nede på jorden -> så hopper man + hoppe animation
+        if (isGrounded == true && Input.GetButtonDown("Jump"))
         {
             isJumping = true;
             jumpTimeCounter = jumpTime;
@@ -59,7 +64,8 @@ public class PlayerMovement : MonoBehaviour {
             anim.SetInteger("state", 2);
         }
 
-        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        //Hvis man hopper i forvejen og holder hoppe knappen nede -> så fortsætter man med at ryge op ad, til timeren ryger ud.
+        if (Input.GetButton("Jump") && isJumping == true)
         {
             if (jumpTimeCounter > 0)
             {
@@ -72,11 +78,14 @@ public class PlayerMovement : MonoBehaviour {
                 isJumping = false;
             }
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+
+        //Hvis man har givet slip på knappen -> så ryger man ned igen
+        if (Input.GetButtonUp("Jump"))
         {
             isJumping = false;
         }
 
+        //Hvis man er i luften og er færdig med at hoppe -> så falder man
         if(isJumping == false && isGrounded == false)
         {
             anim.SetInteger("state", 3);
